65c8dcae0fb664aef42e6e3b9870c956274e96d9
Nicolas Lopez <nicolas.lopez@experis.es>
2021-12-14 09:26:16 +0100

    Initial Commit

---
+++ AppItunes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppItunes
+//
+//  Created by Nicolás López Cano on 14/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppItunesTests/AppItunesTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppItunesTests.swift
+//  AppItunesTests
+//
+//  Created by Nicolás López Cano on 14/12/21.
+//
+
+import XCTest
+@testable import AppItunes
+
+class AppItunesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
