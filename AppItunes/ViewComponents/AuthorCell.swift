abb1b9593f549e04cc45e39ad558da983ebc6d96
Nicolas Lopez <nicolas.lopez@experis.es>
2021-12-14 11:06:41 +0100

    NEW: Created cell

---
+++ AppItunes/ViewComponents/AuthorCell.swift
@@ -0,0 +1,23 @@
+//
+//  AuthorCell.swift
+//  AppItunes
+//
+//  Created by Nicolás López Cano on 14/12/21.
+//
+
+import UIKit
+
+class AuthorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
