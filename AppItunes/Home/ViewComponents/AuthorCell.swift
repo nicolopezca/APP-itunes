8ec7742ee20569d1efa5e7fb213859ce74b0c7d6
Nicolas Lopez <nicolas.lopez@experis.es>
2021-12-16 09:14:13 +0100

    NEW: Cell changed first part

--- AppItunes/Home/ViewComponents/AuthorCell.swift
+++ AppItunes/Home/ViewComponents/AuthorCell.swift
@@ -8,16 +8,34 @@
 import UIKit
 
 class AuthorCell: UITableViewCell {
-    @IBOutlet private weak var authorLabel: UILabel!
-    @IBOutlet private weak var styleLabel: UILabel!
+    @IBOutlet private weak var labelsStack: UIStackView!
     static let cellReuseIdentifier = "AuthorCell"
 
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
-    func setViewModel(_ viewModel: ItunesAuthorViewModel) {
-        self.authorLabel.text = viewModel.author
-        self.styleLabel.text = viewModel.style
+    func setViewModel(_ viewModel: CompletedViewModel) {
+        if let author = viewModel.author {
+            addLabel(author)
+        }
+        if let style = viewModel.style {
+            addLabel(style)
+        }
+        if let firstDisk = viewModel.firstDisc {
+            let discografia = "Discografia"
+            addLabel(discografia)
+            addLabel(firstDisk)
+        }
+        if let secondDisk = viewModel.secondDisc {
+            addLabel(secondDisk)
+        }
+    }
+    
+    func addLabel(_ text: String) {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = text
+        labelsStack.addArrangedSubview(label)
     }
 }
---
+++ AppItunes/Home/ViewModel/CompletedViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CompletedViewModel.swift
+//  AppItunes
+//
+//  Created by Nicolás López Cano on 15/12/21.
+//
+
+import Foundation
+struct CompletedViewModel {
+    let author: String?
+    let style: String?
+    let firstDisc: String?
+    let secondDisc: String?
+}
