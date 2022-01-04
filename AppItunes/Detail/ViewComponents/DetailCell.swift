21a1720ef2febd1c70327367f3f1d6ddc6e4a573
Nicolas Lopez <nicolas.lopez@experis.es>
2022-01-04 10:15:28 +0100

    New: create skeleton

---
+++ AppItunes/Detail/ViewComponents/DetailCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailCell.swift
+//  AppItunes
+//
+//  Created by Nicolás López Cano on 4/1/22.
+//
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
