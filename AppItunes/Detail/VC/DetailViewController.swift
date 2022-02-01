77df1458d1c7842bc81881cb2527b66a3b21ad44
Nicolas Lopez <nicolas.lopez@experis.es>
2022-02-01 13:23:13 +0100

    New: Cell fixed

--- AppItunes/Detail/VC/DetailViewController.swift
+++ AppItunes/Detail/VC/DetailViewController.swift
@@ -8,15 +8,11 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet private weak var detailView: DetailView!
     var artist: Artist?
-    private lazy var detailView: DetailView = {
-        let view = DetailView(frame: self.view.frame)
-        return view
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addDetailView()
         setTitle()
         getDiscography()
     }
@@ -29,13 +25,5 @@ private extension DetailViewController {
     func getDiscography() {
         guard let id = artist?.artistId else { return }
         detailView.getDiscographyFromId(id)
-    }
-    
-    func addDetailView() {
-        self.view.addSubview(detailView)
-        detailView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        detailView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        detailView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        detailView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
 }
